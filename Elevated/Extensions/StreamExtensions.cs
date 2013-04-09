c2f61156a78a4fcca9863321145ba848439531d8
Christopher McGinnis <cmcginnis@gmail.com>
2013-04-09 00:23:33 -0700

    added stream extension

---
+++ Elevated/Extensions/StreamExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+public static class StreamExtensions
+{
+	public static byte[] ToBytes(this Stream stream)
+	{
+		return ToBytes(stream, true);
+	}
+
+	public static byte[] ToBytes(this Stream stream, bool entireStream)
+	{
+		var pos = stream.Position;
+
+		if (entireStream)
+		{
+			stream.Position = 0;
+		}
+
+		var data = new byte[stream.Length - stream.Position];
+
+		stream.Read(data, 0, data.Length);
+		stream.Position = pos;
+		
+		return data;		
+	}
+}
+
