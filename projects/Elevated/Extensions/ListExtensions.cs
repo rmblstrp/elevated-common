f54bd09a131ba4f98210385e369f551be074d506
Christopher McGinnis <cmcginnis@gmail.com>
2013-08-07 23:05:36 -0700

    added extension to cast list items as a different type and return a new list

---
+++ projects/Elevated/Extensions/ListExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ListExtensions
+{
+	public static IList<T> ConvertItems<TItem, T>(this IList<TItem> list)
+		where TItem : class
+		where T : class
+	{
+		var result = new List<T>(list.Count);
+
+		for (int index = 0; index < list.Count; index++)
+		{
+			var convert = list[index] as T;
+
+			if (convert == null)
+			{
+				throw new InvalidCastException();
+			}
+
+			result.Add(convert);
+		}
+
+		return result;
+	}
+}
