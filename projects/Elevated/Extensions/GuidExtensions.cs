ed21e1156d27a37ce3b5b13a9d57ba2a1edeaaad
Christopher McGinnis <cmcginnis@gmail.com>
2013-09-05 20:39:18 -0700

    added a guid extension which checks to see if the current guid is empty

---
+++ projects/Elevated/Extensions/GuidExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+	public static class GuidExtensions
+	{
+		public static bool IsEmpty(this Guid guid)
+		{
+			return guid == Guid.Empty;
+		}
+	}
