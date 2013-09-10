61318ef1cf15189fa95b7218cc6c1c780cbfc77d
Christopher McGinnis <cmcginnis@gmail.com>
2013-09-10 08:33:24 -0700

    added static method which ensures that the setting value is set or pulls it out of the config, converts it, then sets it.

---
+++ projects/Elevated/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace Elevated
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Linq;
+
+	public static class Configuration
+	{
+		public static T EnsureSetting<T>(string key, ref T obj)
+		{
+			if (obj == null)
+			{
+				if (!ConfigurationManager.AppSettings.AllKeys.Contains(key))
+				{
+					throw new KeyNotFoundException(string.Format("'{0}' was not found", key));
+				}
+
+				var generic = typeof(T);
+				var type = generic.FullName.StartsWith("System.Nullable") ? generic.GetGenericArguments()[0] : typeof(T);
+
+				var setting = ConfigurationManager.AppSettings[key];
+				obj = (T)Convert.ChangeType(setting, type);
+			}
+
+			return obj;
+		}
+	}
+}
