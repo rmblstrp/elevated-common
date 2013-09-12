b4df21d8f5d53d0d081ee4e2e8107f072351835c
Christopher McGinnis <cmcginnis@gmail.com>
2013-09-11 22:09:57 -0700

    RS-2: implemented a lot of methods from the facebook php sdk and added an http context helper

--- projects/Elevated.Web/Extensions/FacebookExtension.cs
+++ projects/Elevated.Web/Extensions/FacebookExtension.cs
@@ -5,16 +5,42 @@ using Facebook;
 
 public static class FacebookExtension
 {
+	private enum SupportedKeys
+	{
+		State,
+		Code,
+		AccessToken,
+		UserId
+	}
+
+	private static string AccessToken
+	{
+		get { return HttpContext.Current.GetItem<string>("FacebookExtension.AccessToken", true); }
+		set { HttpContext.Current.Items["FacebookExtension.AccessToken"] = value; }
+	}
+
 	public static long GetUser(this FacebookClient client)
 	{
-		dynamic signed = client.GetSignedRequest();
+		return GetUserFromAvailableData(client);
+	}
 
-		if (signed != null)
+	public static string GetAccessToken(FacebookClient client)
+	{
+		var token = AccessToken;
+
+		if (token.IsNotNullOrEmpty())
 		{
-			return signed.user_id == null ? 0 : Convert.ToUInt64(signed.user_id);
+			return token;
 		}
 
-		return 0;
+		AccessToken = GetApplicationAccessToken(client);
+		token = GetUserAccessToken(client);
+		if (token.IsNotNullOrEmpty())
+		{
+			AccessToken = token;
+		}
+
+		return AccessToken;
 	}
 
 	public static object GetSignedRequest(this FacebookClient client)
@@ -40,13 +66,132 @@ public static class FacebookExtension
 		return client.ParseSignedRequest(signed);
 	}
 
-	public static string SignedRequestCookieName(this FacebookClient client)
+	private static long GetUserFromAvailableData(FacebookClient client)
+	{
+		dynamic signed = client.GetSignedRequest();
+		long userId;
+
+		if (signed != null)
+		{
+			if (signed.user_id != null)
+			{
+				userId = signed.user_id;
+
+				if (userId != GetPersistentData<long>(client, SupportedKeys.UserId))
+				{
+					ClearAllPersistentData(client);
+				}
+
+				SetPersistentData(client, SupportedKeys.UserId, userId);
+
+				return userId;
+			}
+		}
+
+		userId = GetPersistentData<long>(client, SupportedKeys.UserId);
+		var persistedAccessToken = GetPersistentData<string>(client, SupportedKeys.AccessToken);
+
+		var accessToken = GetAccessToken(client);
+
+		if (accessToken.IsNotNullOrEmpty()
+			&& accessToken != GetApplicationAccessToken(client)
+			&& !(userId != long.MinValue && persistedAccessToken == accessToken))
+		{
+			userId = GetUserFromAccessToken(client);
+
+			if (userId != long.MinValue)
+			{
+				SetPersistentData(client, SupportedKeys.UserId, userId);
+			}
+			else
+			{
+				ClearAllPersistentData(client);
+			}
+		}
+
+		return userId;
+	}
+
+	private static long GetUserFromAccessToken(FacebookClient client)
+	{
+		try
+		{
+			dynamic result = client.Get("/me");
+			
+			return result.id;
+		}
+		catch { }
+
+		return long.MinValue;
+	}
+
+	private static string GetUserAccessToken(FacebookClient client)
+	{
+		return null;
+	}
+
+	private static string SignedRequestCookieName(this FacebookClient client)
 	{
 		return "fbsr_" + client.AppId;
 	}
 
-	public static string ApplicationAccessToken(this FacebookClient client)
+	private static string GetApplicationAccessToken(FacebookClient client)
 	{
 		return string.Format("{0}|{1}", client.AppId, client.AppSecret);
+	}
+
+	private static void SetPersistentData(FacebookClient client, SupportedKeys key, object value)
+	{
+		var name = ConstructSessionVariableName(client, key);
+
+		var context = HttpContext.Current;
+
+		context.Items[name] = value;
+	}
+
+	private static T GetPersistentData<T>(FacebookClient client, SupportedKeys key)
+	{
+		var name = ConstructSessionVariableName(client, key);
+
+		var context = HttpContext.Current;
+
+		if (context.Items.Contains(key))
+		{
+			return context.GetItem<T>(name);
+		}
+
+		return default(T);
+	}
+
+	private static void ClearPersistentData(FacebookClient client, SupportedKeys key)
+	{
+		var name = ConstructSessionVariableName(client, key);
+
+		var context = HttpContext.Current;
+
+		if (context.Items.Contains(name))
+		{
+			context.Items.Remove(name);
+		}
+	}
+
+	private static void ClearAllPersistentData(FacebookClient client)
+	{
+		string[] supportedKeys = { "State", "Code", "AccessToken", "UserId" };
+
+		foreach (var key in supportedKeys)
+		{
+			var context = HttpContext.Current;
+
+			if (context.Items.Contains(key))
+			{
+				context.Items.Remove(key);
+			}
+		}
+	}
+
+	private static string ConstructSessionVariableName(FacebookClient client, SupportedKeys key)
+	{
+		return string.Format("fb_{0}_{1}", client.AppId, key);
 	}
 }
---
+++ projects/Elevated.Web/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public static class HttpContextExtensions
+{
+	public static T GetItem<T>(this HttpContext context, string key)
+	{
+		return (T)context.Items[key];
+	}
+
+	public static T GetItem<T>(this HttpContext context, string key, bool returnDefault)
+	{
+		T item = (T)context.Items[key];
+
+		if (item == null)
+		{
+			if (returnDefault)
+			{
+				return default(T);
+			}
+			else
+			{
+				throw new KeyNotFoundException(string.Format("Unable to locate '{0}' in the current context.", key));
+			}
+		}
+
+		return item;
+	}
+
+	public static bool IsSecureConnection(this HttpContext context)
+	{
+		string httpHttps = context.Request.ServerVariables["HTTP_HTTPS"];
+
+		return context.Request.IsSecureConnection || (httpHttps != null && httpHttps.Equals("on", StringComparison.CurrentCultureIgnoreCase));
+	}
+}
