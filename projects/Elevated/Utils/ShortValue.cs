c772faed35a4b4f8713be35f40a01e81516974a1
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2013-10-01 21:16:28 -0700

    RS-46: grabbed class on the net and modified it to use a ulong so it would generate values consistently.
    http://kvz.io/blog/2009/06/10/create-short-ids-with-php-like-youtube-or-tinyurl/

---
+++ projects/Elevated/Utils/ShortValue.cs
@@ -0,0 +1,35 @@
+﻿namespace Elevated.Utils
+{
+	using System;
+
+	public static class ShortValue
+	{
+		public static readonly string Alphabet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		public static string Encode(ulong number)
+		{
+			var result = string.Empty;
+
+			for (int count = (int)Math.Floor(Math.Log(number) / Math.Log(Alphabet.Length)); count >= 0; count--)
+			{
+				result += Alphabet[(int)(Math.Floor(number / BcPow(Alphabet.Length, count)) % Alphabet.Length)];
+			}
+
+			return ReverseString(result);
+		}
+
+		private static double BcPow(double a, double b)
+		{
+			return Math.Floor(Math.Pow(a, b));
+		}
+
+		private static string ReverseString(string s)
+		{
+			var arr = s.ToCharArray();
+
+			Array.Reverse(arr);
+
+			return new string(arr);
+		}
+	}
+}
