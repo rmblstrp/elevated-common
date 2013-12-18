d8d9d5f5e03fbe9d6c690116b092111c2ce83cae
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2013-12-17 21:18:58 -0800

    added a new hashing strategy and compare method

--- projects/Elevated/Security/Password.cs
+++ projects/Elevated/Security/Password.cs
@@ -10,7 +10,8 @@
 		public enum HashingStrategy : byte
 		{
 			SHA256 = 0,
-			SHA512 = 1
+			SHA512 = 1,
+			PBKDF2 = 2
 		}
 
 		public static byte[] GenerateSalt(HashingStrategy strategy)
@@ -21,6 +22,7 @@
 			{
 				case HashingStrategy.SHA256: length = 32; break;
 				case HashingStrategy.SHA512: length = 64; break;
+				case HashingStrategy.PBKDF2: length = 64; break;
 			}
 
 			using (var random = new RNGCryptoServiceProvider())
@@ -48,9 +50,26 @@
 					{
 						return sha512.ComputeHash(bytes.ToArray());
 					}
+				case HashingStrategy.PBKDF2:
+					using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt) { IterationCount = 1000 })
+					{
+						return pbkdf2.GetBytes(salt.Length);
+					}
 			}
 
 			throw new NotImplementedException();
+		}
+
+		public static bool AreEqual(byte[] password1, byte[] password2)
+		{
+			var diff = (uint)password1.Length ^ (uint)password2.Length;
+
+			for (int index = 0; index < password1.Length && index < password2.Length; index++)
+			{
+				diff |= (uint)(password1[index] ^ password2[index]);
+			}
+
+			return diff == 0;
 		}
 	}
 }
