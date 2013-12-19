29fe113aff102fb54aa951e14c60f28cfd98cab4
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2013-12-18 20:43:10 -0800

    converted the class from static so which to make it a little easier to use

--- projects/Elevated/Diagnotics/Statsd.cs
+++ projects/Elevated/Diagnotics/Statsd.cs
@@ -5,138 +5,122 @@
 	using System.Net.Sockets;
 	using System.Text;
 
-	public static class Statsd
+	public class Statsd : IDisposable
 	{
-		private static bool initialized = false;
-		private static string host;
-		private static int port;
+		private Random random = new Random();
+		private UdpClient client;
 
-		[ThreadStatic]
-		private static UdpClient client;
-
-		private static UdpClient Client
+		private double sampleRate;
+		public double SampleRate
 		{
-			get
-			{
-				if (!initialized)
-				{
-					throw new Exception("Statsd must be initialized with the host and port before it can be used.");
-				}
-
-				return client ?? (client = new UdpClient(host, port));
-			}
+			get { return sampleRate; }
+			set { sampleRate = value; }
 		}
 
-		[ThreadStatic]
-		private static Random random;
-
-		private static Random Random
-		{
-			get { return random ?? (random = new Random()); }
-		}
-
-		public static void Init(string name)
+		public Statsd(string name)
 		{
 			var parts = System.Configuration.ConfigurationManager.AppSettings[name].Split(',');
 
 			Init(parts[0], int.Parse(parts[1]));
 		}
 
-		public static void Init(string host, int port)
+		public Statsd(string host, int port)
 		{
-			Statsd.host = host;
-			Statsd.port = port;
-
-			initialized = true;
+			Init(host, port);
 		}
 
-		public static bool Gauge(string key, int value)
+		private void Init(string host, int port)
 		{
-			return Gauge(key, value, 1.0);
+			client = new UdpClient(host, port);
 		}
 
-		public static bool Gauge(string key, int value, double sampleRate)
+		public bool Gauge(string key, int value)
+		{
+			return Gauge(key, value, sampleRate);
+		}
+
+		public bool Gauge(string key, int value, double sampleRate)
 		{
 			return Send(sampleRate, String.Format("{0}:{1:d}|g", key, value));
 		}
 
-		public static bool Timing(string key, int value)
+		public bool Timing(string key, int value)
 		{
-			return Timing(key, value, 1.0);
+			return Timing(key, value, sampleRate);
 		}
 
-		public static bool Timing(string key, int value, double sampleRate)
+		public bool Timing(string key, int value, double sampleRate)
 		{
 			return Send(sampleRate, String.Format("{0}:{1:d}|ms", key, value));
 		}
 
-		public static bool Decrement(string key)
+		public bool Decrement(string key)
 		{
-			return Increment(key, -1, 1.0);
+			return Increment(key, -1, sampleRate);
 		}
 
-		public static bool Decrement(string key, int magnitude)
+		public bool Decrement(string key, int magnitude)
 		{
-			return Decrement(key, magnitude, 1.0);
+			return Decrement(key, magnitude, sampleRate);
 		}
 
-		public static bool Decrement(string key, int magnitude, double sampleRate)
+		public bool Decrement(string key, int magnitude, double sampleRate)
 		{
 			magnitude = magnitude < 0 ? magnitude : -magnitude;
 			return Increment(key, magnitude, sampleRate);
 		}
 
-		public static bool Decrement(params string[] keys)
+		public bool Decrement(params string[] keys)
 		{
-			return Increment(-1, 1.0, keys);
+			return Increment(-1, sampleRate, keys);
 		}
 
-		public static bool Decrement(int magnitude, params string[] keys)
-		{
-			magnitude = magnitude < 0 ? magnitude : -magnitude;
-			return Increment(magnitude, 1.0, keys);
-		}
-
-		public static bool Decrement(int magnitude, double sampleRate, params string[] keys)
+		public bool Decrement(int magnitude, params string[] keys)
 		{
 			magnitude = magnitude < 0 ? magnitude : -magnitude;
 			return Increment(magnitude, sampleRate, keys);
 		}
 
-		public static bool Increment(string key)
+		public bool Decrement(int magnitude, double sampleRate, params string[] keys)
 		{
-			return Increment(key, 1, 1.0);
+			magnitude = magnitude < 0 ? magnitude : -magnitude;
+			return Increment(magnitude, sampleRate, keys);
 		}
 
-		public static bool Increment(string key, int magnitude)
+		public bool Increment(string key)
 		{
-			return Increment(key, magnitude, 1.0);
+			return Increment(key, 1, sampleRate);
 		}
 
-		public static bool Increment(string key, int magnitude, double sampleRate)
+		public bool Increment(string key, int magnitude)
+		{
+			return Increment(key, magnitude, sampleRate);
+		}
+
+		public bool Increment(string key, int magnitude, double sampleRate)
 		{
 			string stat = String.Format("{0}:{1}|c", key, magnitude);
 			return Send(stat, sampleRate);
 		}
 
-		public static bool Increment(int magnitude, double sampleRate, params string[] keys)
+		public bool Increment(int magnitude, double sampleRate, params string[] keys)
 		{
 			return Send(sampleRate, keys.Select(key => String.Format("{0}:{1}|c", key, magnitude)).ToArray());
 		}
 
-		private static bool Send(String stat, double sampleRate)
+		private bool Send(String stat, double sampleRate)
 		{
 			return Send(sampleRate, stat);
 		}
 
-		private static bool Send(double sampleRate, params string[] stats)
+		private bool Send(double sampleRate, params string[] stats)
 		{
 			var retval = false; // didn't send anything
 			if (sampleRate < 1.0)
 			{
 				foreach (var stat in stats)
 				{
-					if (Random.NextDouble() <= sampleRate)
+					if (random.NextDouble() <= sampleRate)
 					{
 						var statFormatted = String.Format("{0}|@{1:f}", stat, sampleRate);
 						if (DoSend(statFormatted))
@@ -160,12 +144,20 @@
 			return retval;
 		}
 
-		private static bool DoSend(string stat)
+		private bool DoSend(string stat)
 		{
 			var data = Encoding.Default.GetBytes(stat + "\n");
 
 			client.Send(data, data.Length);
 			return true;
+		}
+
+		public void Dispose()
+		{
+			if (client != null)
+			{
+				client.Close();
+			}
 		}
 	}
 }
