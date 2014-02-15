870606feccd39b272eaf9a9f0332f3bc2ad64a71
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2014-02-15 00:03:47 -0800

    added compression extensions

---
+++ projects/Elevated/Extensions/CompresionExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class CompresionExtensions
+{
+	public static byte[] Deflate(this string data)
+	{
+		return Encoding.UTF8.GetBytes(data).Deflate();
+	}
+
+	public static byte[] Deflate(this byte[] data)
+	{
+		using (MemoryStream output = new MemoryStream())
+		{
+			using (DeflateStream gzip = new DeflateStream(output, CompressionMode.Compress))
+			{
+				using (StreamWriter writer = new StreamWriter(gzip, System.Text.Encoding.UTF8))
+				{
+					gzip.Write(data, 0, data.Length);
+				}
+			}
+
+			return output.ToArray();
+		}
+	}
+
+	public static string InflateString(this byte[] data)
+	{
+		return Encoding.UTF8.GetString(data.Inflate());
+	}
+
+	public static byte[] Inflate(this byte[] data)
+	{
+		using (MemoryStream input = new MemoryStream(data))
+		{
+			input.Write(data, 0, data.Length);
+			input.Position = 0;
+
+			using (DeflateStream gzip = new DeflateStream(input, CompressionMode.Decompress))
+			{
+				using (MemoryStream output = new MemoryStream())
+				{
+					var buffer = new byte[4096]; int count;
+
+					while ((count = gzip.Read(buffer, 0, buffer.Length)) > 0)
+					{
+						output.Write(buffer, 0, count);
+					}
+
+					return output.ToArray();
+				}
+			}
+		}
+	}
+}
