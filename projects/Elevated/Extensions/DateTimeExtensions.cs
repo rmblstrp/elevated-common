5b17747da19e45eedf2b22ff6f9f05221cf64f9f
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2014-05-24 12:50:06 -0700

    added extensions which make it easier to serialize and deserialize dates for storage in redis

---
+++ projects/Elevated/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class DateTimeExtensions
+{
+	public static byte[] ToBytes(this DateTime value)
+	{
+		return BitConverter.GetBytes(value.Ticks);
+	}
+
+	public static DateTime ToDateTime(this byte[] value, int index = 0)
+	{
+		return new DateTime(BitConverter.ToInt64(value, index));
+	}
+
+	public static DateTime ToDateTime(this byte[] value, DateTimeKind kind = DateTimeKind.Unspecified)
+	{
+		return value.ToDateTime(0, kind);
+	}
+
+	public static DateTime ToDateTime(this byte[] value, int index, DateTimeKind kind = DateTimeKind.Unspecified)
+	{
+		return new DateTime(BitConverter.ToInt64(value, index), kind);
+	}
+}
