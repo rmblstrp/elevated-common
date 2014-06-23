deb2bf1408190248ae2b53da6c5bd4f87730cc5e
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2014-06-23 08:49:27 -0700

    updated encode/decode to use string index instead of substring and added a pad parameter

--- projects/Elevated/Utils/ShortValue.cs
+++ projects/Elevated/Utils/ShortValue.cs
@@ -6,53 +6,71 @@
 	{
 		public static readonly string Alphabet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-		public static ulong Decode(string value)
+		private static decimal BcPow(double a, double b)
 		{
+			return Math.Floor((decimal)Math.Pow(a, b));
+		}
+
+		public static ulong Decode(string value, int pad = 0)
+		{
+			value = value.ReverseString();
 			var len = value.Length - 1;
 			ulong result = 0;
 
 			for (int t = len; t >= 0; t--)
 			{
 				var bcp = (ulong)BcPow(Alphabet.Length, len - t);
-				result += (ulong)Alphabet.IndexOf(value.Substring(t, 1)) * bcp;
+				result += (ulong)Alphabet.IndexOf(value[t]) * bcp;
+			}
+
+			if (pad > 0)
+			{
+				result -= (ulong)BcPow(Alphabet.Length, pad);
 			}
 
 			return result;
 		}
 
-		public static string Encode(byte[] value, int startIndex = 0)
+		public static string Encode(byte[] value, int startIndex = 0, int pad = 0)
 		{
-			return Encode(BitConverter.ToUInt64(value, startIndex));
+			return Encode(BitConverter.ToUInt64(value, startIndex), pad);
 		}
 
-		public static string Encode(Guid guid)
+		public static string Encode(Guid guid, int pad = 0)
 		{
 			var bytes = guid.ToByteArray();
 
-			var first = Encode(bytes, 0);
-			var second = Encode(bytes, 8);
+			var first = Encode(bytes, 0, pad);
+			var second = Encode(bytes, 8, pad);
 
 			return first + second;
 		}
 
-		public static string Encode(ulong value)
+		public static string Encode(ulong value, int pad = 0)
 		{
 			var result = string.Empty;
+
+			if (pad > 0)
+			{
+				value += (ulong)BcPow(Alphabet.Length, pad);
+			}
 
 			for (var t = (value != 0 ? Math.Floor(Math.Log(value, Alphabet.Length)) : 0); t >= 0; t--)
 			{
 			  var bcp = (ulong)BcPow(Alphabet.Length, t);
 			  var a = ((ulong)Math.Floor((decimal)value / (decimal)bcp)) % (ulong)Alphabet.Length;
-			  result += Alphabet.Substring((int)a, 1);
+			  result += Alphabet[(int)a];
 			  value  = value - (a * bcp);
 			}
 
-			return result;
-		}
+			return result.ReverseString();
+		}		
 
-		private static decimal BcPow(double a, double b)
+		private static string ReverseString(this string value)
 		{
-			return Math.Floor((decimal)Math.Pow(a, b));
+			char[] arr = value.ToCharArray();
+			Array.Reverse(arr);
+			return new string(arr);
 		}
 	}
 }
